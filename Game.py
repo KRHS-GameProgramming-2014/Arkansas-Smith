1359d42c2e9fbcfb384fc2416c6b3a5f84f85c09
noahtckr <noah.tucker@kearsarge.org>
2015-04-07 13:21:54 -0400

    Got button working

--- Game.py
+++ Game.py
@@ -1,1 +1,46 @@
+import pygame, sys
+from Button import Button
 
+pygame.init()
+
+clock = pygame.time.Clock()
+
+width = 800 
+height = 600
+size = width, height
+
+screen = pygame.display.set_mode(size)
+
+run = False
+
+startButton = Button([width/2, height-100], 
+                     "Art/Button/Start_Base.png", 
+                     "Art/Button/Start_Clicked.png")
+
+while True:
+    while not run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    run = True
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                startButton.click(event.pos)
+            if event.type == pygame.MOUSEBUTTONUP:
+                if startButton.release(event.pos):
+                    run = True
+        bgImage = pygame.image.load("BL1.png")
+        bgRect = bgImage.get_rect()
+        screen.blit(bgImage, bgRect)
+        screen.blit(startButton.image, startButton.rect)
+        pygame.display.flip()
+        clock.tick(60)
+        
+    while run:
+        bgImage = pygame.image.load("BL1.png")
+        bgRect = bgImage.get_rect()
+        screen.blit(bgImage, bgRect)
+        pygame.display.flip()
+        clock.tick(60)
+        
+        
