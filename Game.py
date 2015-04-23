e8671c1dfd0a32c40a1b89581a39d6034e317943
noahtckr <noah.tucker@kearsarge.org>
2015-04-23 12:55:23 -0400

    dsf

--- Game.py
+++ Game.py
@@ -13,9 +13,12 @@ pygame.init()
 
 clock = pygame.time.Clock()
 
-width = 800  
+width = 800 
 height = 600
 size = width, height
+
+
+bgColor = r,g,b = 0, 0, 10
 
 screen = pygame.display.set_mode(size)
 
@@ -26,6 +29,7 @@ balls = pygame.sprite.Group()
 players = pygame.sprite.Group()
 player1 = Player1(self,args)
 player2 = Player2(self,args)
+
 hudItems = pygame.sprite.Group()
 backgrounds = pygame.sprite.Group()
 blocks = pygame.sprite.Group()
@@ -75,6 +79,55 @@ while True:
     timerWaitMax = 6
 
     score = Score([width-80, height-25], "Score: ", 36)
+        
+    while run:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    player.go("up")
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    player.go("right")
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    player.go("down")
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    player.go("left")
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.K_w or event.key == pygame.K_UP:
+                    player.go("stop up")
+                if event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                    player.go("stop right")
+                if event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                    player.go("stop down")
+                if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                    player.go("stop left")
+            
+        if len(balls) < 10:
+            if random.randint(0, 1*60) == 0:
+                Ball("images/Ball/ball.png",
+                          [random.randint(0,10), random.randint(0,10)],
+                          [random.randint(100, width-100), random.randint(100, height-100)])
+                          
+                          
+        if timerWait < timerWaitMax:
+            timerWait += 1
+        else:
+            timerWait = 0
+            timer.increaseScore(.1)
+        
+        playersHitBalls = pygame.sprite.groupcollide(players, balls, False, True)
+        ballsHitBalls = pygame.sprite.groupcollide(balls, balls, False, False)
+        
+        for player in playersHitBalls:
+            for ball in playersHitBalls[player]:
+                score.increaseScore(1)
+                
+        for bully in ballsHitBalls:
+            for victem in ballsHitBalls[bully]:
+                bully.collideBall(victem)
+        
+        all.update(width, height)
+
         
     while run:
         for event in pygame.event.get():
