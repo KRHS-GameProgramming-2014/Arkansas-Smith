4b4b6e67ae23340fbf53b5375634832f961f1478
Jonny <jonathan.richter@kearsarge.org>
2015-06-01 13:12:19 -0400

    added exit block.png

--- StartBlock.py
+++ StartBlock.py
@@ -3,7 +3,7 @@ import pygame
 class StartBlock(pygame.sprite.Sprite):
 	def __init__(self, pos = [0,0]):
 		pygame.sprite.Sprite.__init__(self, self.containers)
-		self.image = pygame.image.load("Art/Block.png")
+		self.image = pygame.image.load("Art/EnterBlock.png")
 		self.rect = self.image.get_rect()
 		self.place(pos)
 		self.living = True
